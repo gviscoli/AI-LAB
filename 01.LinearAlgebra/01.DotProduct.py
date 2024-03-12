0c05df3f3f9e2e8dfb74c761ef899845b5f61f5c
Giuseppe Viscoli <gviscolio@hotmail.com>
2024-03-12 15:26:28 +0100

    .

--- 01.LinearAlgebra/01.DotProduct.py
+++ 01.LinearAlgebra/01.DotProduct.py
@@ -5,32 +5,45 @@ import array
 
 # 8 bytes size int 
 a = array.array('q') 
-for i in range(100000): 
+for i in range(1000000): 
     a.append(i); 
   
 b = array.array('q') 
-for i in range(100000, 200000): 
+for i in range(1000000, 2000000): 
     b.append(i) 
 
       
 # dot product of vectors implementation  
 # loop version
 #
-tic = time.process_time() 
-dot = 0.0; 
+startTime = time.process_time() 
+dot = 0; 
 
-for i in range(len(a)): 
+for i in range(len(a)):
       dot += a[i] * b[i] 
   
-toc = time.process_time() 
-  
+endTime = time.process_time() 
+
+print("Start time:", startTime)
+print("End time:", endTime)
+
 print("dot_product = "+ str(dot)); 
-print("Computation time = " + str(1000*(toc - tic )) + "ms") 
-   
-  
+print("Computation Start time = " + str(1000*(endTime - startTime)) + "ms") 
+print("Computation End time = " + str((endTime - startTime))) 
+
+
+print("\n")
+
 n_tic = time.process_time() 
 n_dot_product = numpy.dot(a, b) 
 n_toc = time.process_time() 
-  
+
+print("n_tic:", n_tic)
+print("n_toc:", n_toc)
+
 print("\nn_dot_product = "+str(n_dot_product)) 
-print("Computation time = "+str(1000*(n_toc - n_tic ))+"ms") 
+print("Computation Start time = "+str(1000*(n_toc - n_tic ))+"ms") 
+print("Computation End time = "+str((n_toc - n_tic ))) 
+
+
+print("\nTest difference: ",dot-n_dot_product)
