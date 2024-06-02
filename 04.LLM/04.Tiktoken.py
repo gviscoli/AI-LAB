8d3c0725418f4031f475eacd4c338bfeb4a00426
Giuseppe Viscoli <gviscolio@hotmail.com>
2024-06-02 13:55:16 +0200

    .

---
+++ 04.LLM/04.Tiktoken.py
@@ -0,0 +1,12 @@
+import tiktoken
+import os
+
+encoding = tiktoken.encoding_for_model("gpt-2")
+
+a = encoding.encode("Il gatto nero attraversa la strada buia.")
+
+os.system('cls')
+
+print(a)
+print("\n")
+print(encoding.decode(a))
